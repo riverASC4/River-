8bc4ae7944b53842f1060a6a9353ba07529c1594
RiverASC <rivermfields@gmail.com>
2017-07-18 14:51:55 -0400

    bounce

---
+++ River-bounce/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+    createCanvas(500, 400);
+    background(150);
+}
+
+
+
+var xPos = 0
+var yPos = 0
+var x = 3
+var y = 7
+
+
+function draw() {
+    background(150);
+    if(xPos < 0 || yPos > 500) x=-x;
+    if(yPos < 0 || yPos > 500
+
+    
+    ellipse(xPos, yPos, 86);
+   
+    
+  
+
+
+  
