75ab7404b1ba214154daefc2586aa35a82ea0f56
RiverASC <rivermfields@gmail.com>
2018-04-11 13:33:20 -0400

    commit message

---
+++ CodeLabHackWebAPP/js/script.js
@@ -0,0 +1,10 @@
+$(".lead-titles").fadeIn(3000);
+$("#txt3").delay(1000).fadeIn(3000);
+$(".mb-51").delay(1000).fadeIn(3000);
+$("#aboutheader").fadeIn(3000);
+$("#p").delay(1000).fadeIn(3000);
+$("#p2").delay(1000).fadeIn(3000);
+$("#gif").delay(2000).fadeIn(3000);
+
+
+
