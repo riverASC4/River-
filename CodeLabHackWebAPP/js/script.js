2a121871ea97326319808d790893d0e4d9b58bb4
RiverASC <rivermfields@gmail.com>
2018-04-24 09:03:21 -0400

    CodeLabHackWebAPP

--- CodeLabHackWebAPP/js/script.js
+++ CodeLabHackWebAPP/js/script.js
@@ -1,10 +1,12 @@
 $(".lead-titles").fadeIn(3000);
+$("#gify").delay(600).fadeIn(3000);
 $("#txt3").delay(1000).fadeIn(3000);
-$(".mb-51").delay(1000).fadeIn(3000);
 $("#aboutheader").fadeIn(3000);
 $("#p").delay(1000).fadeIn(3000);
 $("#p2").delay(1000).fadeIn(3000);
 $("#gif").delay(2000).fadeIn(3000);
+$("#gify").delay(600).fadeIn(2000);
+
 
 
 
---
+++ JSPromises/main.js
@@ -0,0 +1,11 @@
+function setup () {
+    createCanvas(1300,744);
+    background(260,182);
+}
+
+function draw () {
+    fill("red");
+    ellipse(60,40,40,60);
+    position(400,900);
+}
+Q   A
